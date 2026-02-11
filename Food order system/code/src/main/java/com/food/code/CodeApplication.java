ddc4129143af6d8c4159ac1d95dcebdf729735bd
Ekta Sahu <83973342+Ektasa@users.noreply.github.com>
2026-02-11 23:35:31 +0530

    Erueka

---
+++ Food order system/code/src/main/java/com/food/code/CodeApplication.java
@@ -0,0 +1,15 @@
+package com.food.code;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class CodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeApplication.class, args);
+	}
+
+}
